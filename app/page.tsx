4c0f0315e4115c3909895e367d4e06ed39d86d53
karandeep4423 <karanhanju9696@gmail.com>
2025-08-07 19:32:10 +0200

    added domain

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function LandingPage() {
     };
 
     try {
-      const res = await fetch("/api/send-email", {
+      const res = await fetch("https://www.monhubimmo.com/api/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
